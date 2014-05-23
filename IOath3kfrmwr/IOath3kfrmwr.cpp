9e5b06ce598867ea638e715818b49141d492677d
RehabMan <racerrehabman@gmail.com>
2014-05-23 14:24:51 -0700

    build fix for recent Xcode 5

--- IOath3kfrmwr/IOath3kfrmwr.cpp
+++ IOath3kfrmwr/IOath3kfrmwr.cpp
@@ -17,6 +17,15 @@
 #include "ath3k-1fw.h"
 #endif
 
+//REVIEW: avoids problem with Xcode 5.1.0 where -dead_strip eliminates these required symbols
+#include <libkern/OSKextLib.h>
+void* _org_rehabman_dontstrip_[] =
+{
+    (void*)&OSKextGetCurrentIdentifier,
+    (void*)&OSKextGetCurrentLoadTag,
+    (void*)&OSKextGetCurrentVersionString,
+};
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
 #define USB_REQ_DFU_DNLOAD	1
